00fb3fc147131dc8e457f6e829ffc4e062aa7cff
Hoàng Nam Lê <91420375+hoangnam-3@users.noreply.github.com>
2021-10-01 23:15:02 +0700

    Add files via upload

---
+++ EmployeeManagement/EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int employeeId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public Gender gender { get; set; }
+        public Department department { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string photoPath { get; set; }
+    }
+}
