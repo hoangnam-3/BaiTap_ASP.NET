f71d89a5d31cf0ef85231912e99ef6dd8915614d
HOANGNAM\Asus <lhnam.th113@kgc.edu.vn>
2021-10-02 19:16:45 +0700

    a

---
+++ Employee_Again/Employee.Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee.Models
+{
+    public class Department
+    {
+        public string departmentName { get; set; }
+        public int departmentId { get; set; }
+    }
+}
