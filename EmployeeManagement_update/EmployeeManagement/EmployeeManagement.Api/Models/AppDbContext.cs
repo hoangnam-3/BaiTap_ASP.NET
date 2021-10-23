9852d7fe9f78e14af0ddf5c31cdf34689cb0c6a1
Hoàng Nam Lê <lhnam.th113@kgc.edu.vn>
2021-10-23 21:22:35 +0700

    abc

---
+++ EmployeeManagement_update/EmployeeManagement/EmployeeManagement.Api/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees3 { get; set; }
+    }
+}
---
+++ EmployeeManagement_update/EmployeeManagement/EmployeeManagement.Web/Pages/EmployeeDetaislBase.cs
@@ -0,0 +1,63 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class EmployeeDetaislBase : ComponentBase
+    {
+        public Employee Employee { get; set; } = new Employee();
+
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+
+        protected string CssClass { get; set; } = null;
+
+        public int plus { get; set; } = 1;
+
+        [Inject]
+        IEmployeeService EmployeeServices { get; set; }
+        [Parameter]
+        public string Id { get; set; }
+        protected override async Task OnInitializedAsync()
+        {
+            Id = Id ?? "1";
+            Employee = await EmployeeServices.GetEmployee(int.Parse(Id));
+        }
+
+        protected void Mouse_Move(MouseEventArgs e)
+        {
+            Coordinates = $"X= {e.ClientX} Y = {e.ClientY}";
+        }
+
+        protected void ButtonClick()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "Hide Footer";
+            }
+            else
+            {
+                ButtonText = "Hide Footer";
+                CssClass = null;
+            }
+        }
+
+        protected void BtnClick1()
+        {
+            plus++;
+        }
+        protected void BtnClick2()
+        {
+            if (plus > 1)
+                plus--;
+        }
+
+    }
+}
